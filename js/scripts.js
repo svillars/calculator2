6f9749b2568ca46265e4f728a2b61272e95156c1
Renee Mei and Shane Villars <shane@>
2017-01-11 11:35:52 -0800

    add front-end logic and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,58 @@
-var add = function(number1,number2){
-	return number1+number2
-};
+//Business (Back-End) Logic:
 
-var sub = function(number1,number2){
-	return number1-number2
-};
+	var add = function(number1,number2){
+		return number1+number2;
+	};
 
-var multiply = function(number1,number2){
-	return number1*number2
-};
+	var sub = function(number1,number2){
+		return number1-number2;
+	};
 
-var divide = function(number1,number2){
-	return number1/number2
-};
+	var multiply = function(number1,number2){
+		return number1*number2;
+	};
 
+	var divide = function(number1,number2){
+		return number1/number2;
+	}
+//User-Interface Logic (Front-End) Logic:
 
-var number1= prompt("Enter a number:");
-var number2= prompt("Enter another number:");
+	$(document).ready(function(){
 
-number1= parseInt(number1);
-number2= parseInt(number2);
+		$("form#add").submit(function(event){
 
-var result = divide(number1,number2);
-alert(result);
+			event.preventDefault();
+			var number1= parseInt($("#add1").val());
+			var number2= parseInt($("#add2").val());
+			var result = add(number1, number2);
+			$("#answer").text(result);
+		})
+
+		$("form#subtract").submit(function(event){
+
+			event.preventDefault();
+			var number1= parseInt($("#subtract1").val());
+			var number2= parseInt($("#subtract2").val());
+			var result = sub(number1, number2);
+			$("#answer").text(result);
+		})
+
+		$("form#multiply").submit(function(event){
+
+			event.preventDefault();
+			var number1= parseInt($("#multiply1").val());
+			var number2= parseInt($("#multiply2").val());
+			var result = multiply(number1, number2);
+			$("#answer").text(result);
+		})
+
+		$("form#divide").submit(function(event){
+
+			event.preventDefault();
+			var number1= parseInt($("#divide1").val());
+			var number2= parseInt($("#divide2").val());
+			var result = divide(number1, number2);
+			$("#answer").text(result);
+		})
+
+	});
